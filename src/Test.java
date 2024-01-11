9c66eb1b72d64a27844d76816f3f6b3c8224a1e5
= <=>
2024-01-11 21:29:42 +0600

    Версия 1

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,11 @@
 public class Test {
     public static void main(String[] args) {
         // Версия 1
+
+        // git init - создание локального репозитория
+        // git add - добавить файлы в staging area
+        // git commit -m "текст" - фиксация изменений
+
+        // git status - просмотр состояния локального репозитория
     }
 }
